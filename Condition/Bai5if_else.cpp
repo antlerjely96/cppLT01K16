0b3ac79a85a0c46f5127e92c30cf0dabd066f36c
antlerjely96 <antlerjely96@gmail.com>
2024-10-17 16:46:22 +0700

    Add demo condition Bai5 (code (if-else, switch-sace))

--- Condition/Bai5if_else.cpp
+++ Condition/Bai5if_else.cpp
@@ -1,4 +1,36 @@
 /*
-	5. Nhap 1 so nguyen n.
-	Hien thi so ngay trong thang n. Neu n == 2 thi cho nhap nam va hien thi so ngay
+	5. Nhap 1 so nguyen m.
+	Hien thi so ngay trong thang m. Neu m == 2 thi cho nhap y va hien thi so ngay cua thang m nam y
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Khai bao m
+	int m;
+	//Nhap m
+	cout << "Thang: ";
+	cin >> m;
+	//Kiem tra m
+	if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+		cout << "Thang co 31 ngay" << endl;
+	} else if (m == 4 || m == 6 || m == 9 || m == 11){
+		cout << "Thang co 30 ngay" << endl;
+	} else if (m == 2) {
+		//Khai bao y
+		int y;
+		//Nhap y
+		cout << "Nhap y: ";
+		cin >> y;
+		//Kiem tra y
+		if (y % 4 == 0){
+			cout << "Thang co 29 ngay" << endl;
+		} else {
+			cout << "Thang co 28 ngay" << endl;
+		}
+	} else {
+		cout << "Thang khong ton tai" << endl;
+	}
+	return 0;
+}
--- Condition/Bai5switch_case.cpp
+++ Condition/Bai5switch_case.cpp
@@ -2,3 +2,68 @@
 	5. Nhap 1 so nguyen n.
 	Hien thi so ngay trong thang n. Neu n == 2 thi cho nhap nam va hien thi so ngay
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao m
+	int m;
+	//Nhap m
+	cout << "Thang: ";
+	cin >> m;
+	//switch - case: khi gap 1 case thoa man thi se chay khoi lenh trong case do va cac case ben duoi
+	// Nen trong khoi lenh trong case phai co lenh break de khong chay case tiep theo
+	switch(m){
+		case 1:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 3:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 5:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 7:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 8:
+			cout << "Thang co 31 ngay" << endl;
+			break;	
+		case 10:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 12:
+			cout << "Thang co 31 ngay" << endl;
+			break;
+		case 4:
+			cout << "Thang co 30 ngay" << endl;
+			break;
+		case 6:
+			cout << "Thang co 30 ngay" << endl;
+			break;
+		case 9:
+			cout << "Thang co 30 ngay" << endl;
+			break;
+		case 11:
+			cout << "Thang co 30 ngay" << endl;
+			break;
+		case 2:
+			//Khai bao y
+			int y;
+			//Nhap y
+			cout << "Nhap y: ";
+			cin >> y;
+			//Kiem tra y
+			if (y % 4 == 0){
+				cout << "Thang co 29 ngay" << endl;
+			} else {
+				cout << "Thang co 28 ngay" << endl;
+			}
+			break;
+		default:
+			cout << "Thang khong ton tai" << endl;
+			break;
+	}
+	return 0;
+}
