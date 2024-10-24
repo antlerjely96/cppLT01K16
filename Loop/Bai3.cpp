6ddeec21980e60df422fd5fa37acd3f26b6bb3f9
antlerjely96 <antlerjely96@gmail.com>
2024-10-24 17:21:27 +0700

    add demo Loop Bai3 (code), flowchart Bai4

--- Loop/Bai3.cpp
+++ Loop/Bai3.cpp
@@ -2,3 +2,27 @@
 	3. Nhap 1 so nguyen n. Neu n < 0 thi tinh tong cac so tu n den 0
 		Neu n >= 0 thi tinh tong cac so tu 0 den n
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	//khai bao
+	int n, i, tong = 0;
+	//Nhap n
+	cout << "nhap n: ";
+	cin >> n;
+	if(n < 0){
+		//tinh tong tu n den 0
+	 	for (i = n; i <= 0; i++){
+	 		tong = tong + i;
+		}
+	} else {
+ 		//tinh tong tu 0 den n
+	 	for (i = 0; i <= n; i++){
+	 		tong = tong + i;
+		}
+	}
+	//hien thi ket qua
+	cout << "tong cac so nguyen là: " << tong << endl;
+	return 0;
+}
---
+++ Loop/Bai4.cpp
@@ -0,0 +1,3 @@
+/*
+	4. Nhap so nguyen n. Tinh tong cac so chan trong khoang tu 1 den n (voi n > 0)
+*/
