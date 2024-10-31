0f27261b65ab022fae941c1279c526d73d68baad
antlerjely96 <antlerjely96@gmail.com>
2024-10-31 17:24:04 +0700

    add demo Bai11 (flowchart, code)

--- Loop/Bai11.cpp
+++ Loop/Bai11.cpp
@@ -6,3 +6,18 @@
 	1	2	3	4
 	1	2	3	4	5
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao i, j
+	int i, j;
+	for(i = 1; i <= 5; i++){
+		for(j = 1; j <= i; j++){
+			cout << j << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+}
