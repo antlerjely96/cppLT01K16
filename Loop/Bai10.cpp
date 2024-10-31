3d0cf87a84747701072eca2a00fb0ebd0570fc5d
antlerjely96 <antlerjely96@gmail.com>
2024-10-31 16:42:06 +0700

    add demo Bai10 (flowchart, code)

--- Loop/Bai10.cpp
+++ Loop/Bai10.cpp
@@ -6,3 +6,21 @@
 	*	*	*	*
 	*	*	*	*	*
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao i
+	int i, j;
+	//Chay vong lap in theo chieu doc
+	for(i = 1; i <= 5; i++){
+		//Chay vong lap in theo chieu ngang
+		for(j = 1; j <= i; j++){
+			cout << "*" << "\t";
+		}
+		//Xuong dong
+		cout << endl;
+	}
+	return 0;
+}
