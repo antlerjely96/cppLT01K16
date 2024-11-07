871309c62285c70a511e61adac64065d381db40c
antlerjely96 <antlerjely96@gmail.com>
2024-11-07 15:25:05 +0700

    add demo bai 12, 13, 14, 15

--- Loop/Bai14.cpp
+++ Loop/Bai14.cpp
@@ -6,3 +6,18 @@
 	7	8	9	10
 	11	12	13	14	15
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i, j, count = 1;
+	for (i = 1; i <= 5; i++){
+		for(j = 1; j <= i; j++){
+			cout << count << "\t";
+			count++;
+		}
+		cout << endl;
+	}
+	return 0;
+}
--- Loop/Bai15.cpp
+++ Loop/Bai15.cpp
@@ -6,3 +6,21 @@
 	*				*
 	*	*	*	*	*
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i, j;
+	for (i = 1; i <= 5; i++){
+		for(j = 1; j <= 5; j++){
+			if(i == 1 || i == 5 || j == 1 || j == 5){
+				cout << "*\t";
+			} else {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
