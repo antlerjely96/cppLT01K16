90fba53d33f763c17cf3f84525a93d809850f434
antlerjely96 <antlerjely96@gmail.com>
2024-11-21 16:53:39 +0700

    add flowchart, code Bai5 Array

--- Array/Code/Bai5.cpp
+++ Array/Code/Bai5.cpp
@@ -3,4 +3,27 @@
 	Tinh tong cac phan tu co phan nguyen la so chan
 */
 
+#include <iostream>
+using namespace std;
+
+int main(){
+	//khai bao va nhap n
+	int n;
+	float sum = 0;
+	cout << "nhap so mang : ";
+	cin >> n;
+	//khai bao mang
+	float arr[n];
+	for (int i = 0; i <n; i++){
+		cout << "Nhap arr [" << i << "] = ";
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n; i++){
+		if((int)arr[i] % 2 == 0){
+			sum += arr[i];
+		}
+	}
+	cout << "Tong cac phan tu co phan nguyen chan la: " << sum << endl;
+	return 0;	
+}
 
