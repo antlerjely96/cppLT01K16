c0f3cc5cd36e9c7d9aad6fdcb1b9723ffe5991c2
antlerjely96 <antlerjely96@gmail.com>
2024-11-25 11:56:43 +0700

    add code Bai10 Array

--- Array/Code/Bai10.cpp
+++ Array/Code/Bai10.cpp
@@ -3,3 +3,30 @@
 	Nhap 1 so nguyen m tu ban phim.
 	Hien thi so lan xuat hien cua m trong mang
 */
+
+#include <iostream>
+using namespace std;
+int main(){
+	//Khai bao n,m,arr[n]
+	int n, m;
+	cout << "Nhap n: ";
+	cin >> n;
+	int arr[n];
+	//Nhap arr[i]
+	for(int i = 0; i < n; i++){
+		cout << "arr[" << i << "]: ";
+		cin >> arr[i];
+	}
+	//Nhap m
+	cout << "Nhap m: ";
+	cin >> m;
+	int count = 0;
+	//Chay vong lap de xac dinh so lan xuat hien m
+	for(int i = 0; i < n; i++){
+		if(arr[i] == m){
+			count++;
+		}
+	}
+	//Hien thi so lan xuat hien cua m trong mang
+	cout << count;
+}
