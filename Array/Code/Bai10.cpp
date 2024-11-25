a705d007a66593a6e13979bc3ebb808478d3a038
antlerjely96 <antlerjely96@gmail.com>
2024-11-25 11:17:07 +0700

    add flowchart, code Bai9 Array

---
+++ Array/Code/Bai10.cpp
@@ -0,0 +1,5 @@
+/*
+	10. Nhap mang so nguyen co 10 phan tu.
+	Nhap 1 so nguyen m tu ban phim.
+	Hien thi so lan xuat hien cua m trong mang
+*/
--- Array/Code/Bai9.cpp
+++ Array/Code/Bai9.cpp
@@ -2,3 +2,43 @@
 	9. Nhap mang so nguyen co n phan tu.
 	Sap xep cac phan tu cua mang theo chieu giam dan
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	//Khai bao n, temp
+	int n, temp;
+	//Nhap n
+	cout << "Nhap so phan tu n: ";
+	cin >> n;
+	//Khai bao mang 
+	int arr[n];
+	//Nhap cac phan tu 
+	for (int i = 0; i < n; i++ ){
+		cout <<"Nhap arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+	//Su dung thuat toan sap xep 
+	for (int i = 0; i < n - 1; i++){
+		for (int j = i + 1; j < n; j++){
+			if (arr[i] < arr [j]){
+				temp = arr [i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				
+			}
+		}
+	}
+	//Hien thi ket qua
+	cout << "Cac phan tu sau khi sap xep theo chieu giam dan la:\n";
+	for (int i = 0; i < n; ++i){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
+	
+	
+	
+
