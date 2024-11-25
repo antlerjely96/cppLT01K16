f757cd13eb52d0d562af47394e438d0ec3f98808
antlerjely96 <antlerjely96@gmail.com>
2024-11-25 10:52:15 +0700

    add code Bai8, flowchart Bai9 Array

--- Array/Code/Bai8.cpp
+++ Array/Code/Bai8.cpp
@@ -3,4 +3,36 @@
 	Sap xep cac phan tu cua mang theo chieu tang dan
 */
 
-
+#include <iostream>
+using namespace std;
+int main(){
+	//khai bao
+	int n;
+	//nhap n
+	cout << "Nhap n: ";
+	cin >> n;
+	//khai bao mang
+	float arr[n], temp;
+	//vong lap de nhap mang
+	for(int i = 0; i < n; i ++){
+		cout << "arr[" << i << "]: ";
+		cin >> arr[i];
+	}
+	//vong lap de sap xep mang
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			//kiem tra va hoan doi vi tri neu so dung sau nho hon so phia truoc
+			if(arr[i] > arr[j]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	//in mang ra sau khi sap xep
+	cout << "Mang sau khi sap xep la: ";
+	for ( int i = 0; i < n; i++){
+		cout << arr[i] << "\t";
+	}
+	return 0;
+}
