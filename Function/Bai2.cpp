a4c586e48b0a0d95b5ba554919544eb6fa1295ec
antlerjely96 <antlerjely96@gmail.com>
2024-12-05 14:44:15 +0700

    add de bai Bai2 function

---
+++ Function/Bai2.cpp
@@ -0,0 +1,11 @@
+/*
+	2. Nhap 3 so thuc a, b. Tinh
+	a, a + b + c
+	b, a - b + c
+	c, a * b / c
+	d, a^2 + b^2 + c^2
+	e, a^2 + 2 * a * b + b^2
+	f, a^3 + 3 * a^2 + b + 3 * a * b^2 + b^3
+	g, a * b * c
+	Su dung function de thuc hien
+*/
