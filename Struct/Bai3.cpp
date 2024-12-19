06fadbfb57678c5ae929103f85e44cca7e843c7e
antlerjely96 <antlerjely96@gmail.com>
2024-12-19 17:32:22 +0700

    update de bai 3 struct

--- Struct/Bai3.cpp
+++ Struct/Bai3.cpp
@@ -5,5 +5,5 @@
 	b. Nhap thong tin cho cac bat dong san
 	c. Tinh chu vi, dien tich cua tung bat dong san
 	d. Hien thi thong tin bat dong san co dien tich lon nhat
-	e. Tinh tien mua tung bat dong san, biet gia moi met vuong bang 450.000.000
+	e. Tinh tien mua tung bat dong san, biet gia moi met vuong bang 18.750$
 */
